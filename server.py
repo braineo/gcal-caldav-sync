d56572dfe00d8ac002b0cd783027686df4f0082c
Binbin Ye <binbin.ye@mujin.co.jp>
2020-02-03 16:21:20 +0900

    Implement sync

--- server.py
+++ server.py
@@ -3,10 +3,10 @@ import datetime
 import json
 import os
 import pickle
+import ics
 import googleapiclient.discovery
 import google_auth_oauthlib.flow
 import google.auth.transport.requests
-from caldav.elements import dav, cdav
 import logging
 
 logging.basicConfig()
@@ -53,34 +53,92 @@ class GoogleCalendarClient(object):
             if self._credentials and self._credentials.expired and self._credentials.refresh_token:
                 self._credentials.refresh(google.auth.transport.requests.Request())
             else:
-                flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(client_secrets_file_path, GoogleCalendarClient.SCOPES)
+                flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+                    client_secrets_file_path, GoogleCalendarClient.SCOPES
+                )
                 self._credentials = flow.run_local_server(port=0)
             # Save the credentials for the next run
             with open("token.pickle", "wb") as token:
                 pickle.dump(self._credentials, token)
 
-        self._service = googleapiclient.discovery.build("calendar", "v3", credentials=self._credentials, cache_discovery=False)
+        self._service = googleapiclient.discovery.build(
+            "calendar", "v3", credentials=self._credentials, cache_discovery=False
+        )
+
+    def get_events(self, calendar_name):
+        page_token = None
+        events = self._service.events().list(calendarId=calendar_name, pageToken=page_token).execute()
+        while True:
+            for event in events["items"]:
+                yield event
+            page_token = events.get("nextPageToken")
+            if not page_token:
+                break
 
 
 class EventSynchronizer(object):
 
-    source_caldav = None
-    source_calendar_name = ""
-    target_caldav = None
-    target_calendar_name = ""
+    gcal_client = None
+    gcal_calendar_id = ""
+    caldav_client = None
+    caldav_calendar_name = ""
 
-    def __init__(self, source_caldav, source_calendar_name, target_caldav, target_calendar_name):
-        self.source_caldav = source_caldav
-        self.source_calendar_name = source_calendar_name
-        self.target_caldav = target_caldav
-        self.target_calendar_name = target_calendar_name
+    def __init__(self, gcal_client, gcal_calendar_id, caldav_client, caldav_calendar_name):
+        self.gcal_client = gcal_client
+        self.gcal_calendar_id = gcal_calendar_id
+        self.caldav_client = caldav_client
+        self.caldav_calendar_name = caldav_calendar_name
 
-    def get_syncing_events(self):
-        calendar = self.source_caldav.get_calendar(self.source_calendar_name)
-        return calendar.date_search(start=datetime.datetime.today())
+    def sync_once(self, events, ical_calender):
+        event = EventResource(next(events))
+        ical_event = event.export_ical()
+        try:
+            ical_event = ical_calender.event_by_uid(event.get("iCalUID", ""))
+            log.debug("updating event with UID %r", event.get("iCalUID", ""))
+            ical_event.data = event.export_ical()
+            ical_event.save()
+        except caldav.error.NotFoundError:
+            log.debug("creating event with UID %r", event.get("iCalUID", ""))
+            ical_calender.add_event(ical_event)
+        except Exception as e:
+            log.error("unexpected error %r", e)
 
     def sync(self):
-        pass
+        events = self.gcal_client.get_events(self.gcal_calendar_id)
+        ical_calender = self.caldav_client.get_calendar(self.caldav_calendar_name)
+        try:
+            while True:
+                self.sync_once(self, events, ical_calender)
+        except StopIteration:
+            log.info("finished syncing")
+        except Exception:
+            log.error("cannot sync")
+
+
+class EventResource(dict):
+    def export_ical(self):
+        ics_event = ics.Event(
+            name=self["summary"],
+            begin=self.get("start", {}).get("dateTime", None),
+            end=self.get("end", {}).get("dateTime", None),
+            duration=None,
+            uid=self.get("iCalUID", None),
+            description=self.get("description", None),
+            created=self.get("created", None),
+            last_modified=self.get("updated", None),
+            location=self.get("location", None),
+            url=None,
+            transparent=self.get("transparency", None),
+            alarms=None,
+            attendees=None,
+            categories=None,
+            status=None,
+            organizer=self.get("organizer", {}).get("email", None),
+            geo=None,
+            classification=None,
+        )
+
+        return ics_event.__str__()
 
 
 def main():
