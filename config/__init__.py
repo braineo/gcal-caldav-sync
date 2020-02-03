eb51b6a3c0647737b31c77ca1944475111925704
Binbin Ye <phantom2501@gmail.com>
2020-02-03 22:22:15 +0900

    Add credentials path to config

--- config/__init__.py
+++ config/__init__.py
@@ -1,7 +1,12 @@
+import os
+
 caldav = {
     "username": "username",
     "password": "password",
     "calendar_url": "https://url/to/Calendar/"
 }
 
-gcal = {"calendar_id": "calendar_id of google calender"}
+gcal = {
+    "calendar_id": "calendar_id of google calender",
+    "credentials_path": os.path.join(os.path.dirname(os.path.abspath(__file__)), "credentials.json"),
+}
